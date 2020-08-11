4b593af937da2fd4a02ae42accd5757c2534439a
Deepunyk <nayakd82@yahoo.com>
2020-08-11 15:49:14 +0530

    app and desktop final website with minor change

---
+++ lib/screens/tablet/tablet_main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabletMainScreen extends StatefulWidget {
+  @override
+  _TabletMainScreenState createState() => _TabletMainScreenState();
+}
+
+class _TabletMainScreenState extends State<TabletMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/tablet/tablet_service_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabletServiceScreen extends StatefulWidget {
+  @override
+  _TabletServiceScreenState createState() => _TabletServiceScreenState();
+}
+
+class _TabletServiceScreenState extends State<TabletServiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
